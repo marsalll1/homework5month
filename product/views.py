8f94be10daef1d27a14daac282491bba678de1f5
marsalll1 <alinamarsovna042@gmail.com>
2026-03-06 23:42:08 +0600

    третья  домашка

--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from django.db.models import Avg
 from .models import Category, Product, Review
 from .serializers import (
     CategorySerializer,
@@ -9,34 +8,35 @@ from .serializers import (
 )
 
 
-class CategoryListView(generics.ListAPIView):
+class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class CategoryDetailView(generics.RetrieveAPIView):
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class ProductListView(generics.ListAPIView):
+class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ProductDetailView(generics.RetrieveAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ReviewListView(generics.ListAPIView):
+class ReviewListView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
-class ReviewDetailView(generics.RetrieveAPIView):
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
 
 class ProductReviewsListView(generics.ListAPIView):
     queryset = Product.objects.all()
